609bcd12e11b14a612a8494932080776c5be44e8
Aaron Throckmorton <aaron.throckmorton@stgconsulting.com>
2022-05-18 11:47:14 -0600

    Add publisher to export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as Collector } from './Collector';
 export { default as Parser } from './Parser';
 export { TelemetryRow } from './TelemetryRow';
+export { Publisher } from './Publisher';
